e302fbc4acbd759094adbe6e8fffef7420b2b1c5
joao <lucasribeiromatos18@gmail.com>
2023-08-12 09:13:44 -0300

    new file:   dados/banco-dados.db 	modified:   main.py 	modified:   tarefas.txt 	modified:   test.py

--- main.py
+++ main.py
@@ -1,45 +1,86 @@
-
+import sqlite3
 import playsound
 import customtkinter as ctk
 import os
 
-exetecaoMusica = ('.mp3', '.ogg')
+filtro = ('.mp3', '.ogg')
+janela_size = {'x': 100, 'y': 600}
 
 # Configurando a janela
 janela = ctk.CTk()
 janela.geometry('1000x600')
 janela.title('playsound')
 janela.minsize(600, 400)
+try:
+    conexao = sqlite3.connect('dados/banco-dados.db') 
 
-# Criando menu e configurando
-menu = ctk.CTkFrame(janela, corner_radius=0, height=600)
-menu.grid_anchor('e') # definido o menu fixado na esquerda 
-menu.grid()
+except sqlite3.OperationalError:
+    os.makedirs('dados')
+    conexao = sqlite3.connect('dados/banco-dados.db')
+    curso = conexao.cursor()
+    curso.execute("CREATE TABLE dado(caminho_musica Text)")
 
-
-
-def buscador_musica(pasta):
-    "buscar por todas as musica do diretorio "
-    lista_musicas = []
-    
-    for raiz, *_, arquivos in os.walk(pasta):
-        for arquivo in arquivos:
-            caminho_completo = os.path.join(raiz, arquivo)
-            
-            if any(exetecao in arquivo for exetecao in exetecaoMusica):
-                lista_musicas.append(caminho_completo)
-    
-    lista_musicas.sort()
-    return lista_musicas
-
-# Iniciando a aplicasão            
-class Playlist:
-    def __init__(self):
-        diretorio = '/home'
-        playlist = buscador_musica(diretorio)  
+# Iniciando a aplicasão    
+class App:
+    def __init__(self) -> None:
+        global scroll
+        ctk.CTkButton(janela, text='arquivos', command=self.menuBurg ,width=30, height=20).place(x=0, y=0)
+        scroll = ctk.CTkScrollableFrame(janela, 180, janela_size['y'] - 30)
+        scroll.place(x=0,y=24)
         
-        playsound.playsound(playlist[0])
+
+        if curso.execute("SELECT * FROM dados").fetchall() != []:
+        janela.after(10, self.update)      
+        janela.after(10, self.buscar)
+        janela.mainloop()
+        
+    def buscar(self):
+        "buscar por todas as musica do diretorio"
+        
+        diretorio = '/home'
+        notificasao = ctk.CTkLabel(janela, text='o sistema ficara mais lento e quanto buscar por arquivos...')
+        notificasao.place(x=60, y=0)
+        for raiz, *_, arquivos in os.walk(diretorio):
+            for arquivo in arquivos:
+                caminho_completo = os.path.join(raiz, arquivo)
+                # Filtrando os arquivos 
+                if any(exetecao in arquivo for exetecao in filtro) and curso.execute("SELECT * FROM dados WHERE caminho_musica = ?", caminho_completo) == []:
+                    ctk.CTkButton(scroll, text=arquivo, width=175).pack(pady=2, ipadx=10)
+                    curso.execute("INSERT INTO dados VALUES(caminho_musica = ?)", caminho_completo)
+                    conexao.commit()
+                    scroll.update()
+                    
+                    
+        notificasao.place_forget()
+    def update(self):
+        if janela_size['y'] != janela.winfo_height():             
+            janela_size['y'] = janela.winfo_height()
+            scroll.configure(height=janela_size['y'] -30) 
+        janela.after(1, self.update)
+        
+    def fechar_MenuBurg(self, evento):
+        largura = burg.winfo_width() + burg.winfo_x()
+        altura = burg.winfo_height() + burg.winfo_y()
+        if largura < evento.x or altura < evento.y:
+            janela.unbind('<Button-1>')
+            burg.place_forget()
+
+    def menuBurg(self):
+        global burg
+        janela.bind('<Button-1>', self.fechar_MenuBurg)
+        try: 
+            burg.update()
+        except NameError:
+            burg = ctk.CTkFrame(janela)
+            ctk.CTkButton(burg, 200, text='buscar manualmente').pack(pady=2, ipadx=10)
+            ctk.CTkButton(burg, 200, text='musicas exicluidas').pack(pady=2, ipadx=10)
+            ctk.CTkButton(burg, 200, text='buscar automaticamente', command=self.buscar).pack(pady=2, ipadx=10)
+        else:
+            burg.place_forget()
+
+        burg.place(x=0, y=20)
+        
 
 if __name__ == '__main__':
-    Playlist()
+    App()
     janela.mainloop()
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-bola = [['a','b', 'c'], ['c', 'b', 'a']]
-for i, *_, l in bola:
-    print(i, l)
+import sqlite3
+
+try:
+    conexao = sqlite3.connect('dados/banco-dados.db') 
+except sqlite3.OperationalError:
+    os.makedirs('dados')
+    conexao = sqlite3.connect('dados/banco-dados.db')
+    curso.execute(" CREATE TABLE dados(nome TEXT, idade INTER)")
+curso = conexao.cursor()
+curso.execute('INSERT INTO dados VALUES("joao", 12)')
+print(curso.execute('SELECT * FROM dados WHERE nome = "oao" ').fetchall())
