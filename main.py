e7cdb8c3054eb1fe01708bc6c5deaf98fdb960ab
joao322 <lucasribeiromatos18@gmail.com>
2023-09-03 17:46:14 -0300

    correcao na escrita

--- main.py
+++ main.py
@@ -6,26 +6,31 @@ caminho_banco = 'dados/banco.db'
 if not os.path.exists(caminho_banco):
     os.makedirs('dados')
 conexao = sqlite3.connect(caminho_banco)
-
 cursor = conexao.cursor()
 
-# Criando a tabla se ela nao existe 
-cursor.execute('CREATE TABLE IF NOT EXISTS dados(caminho TEXT, nome TEXT, duracao FLOAT)')
+# Criando a tabela se ela não existe
+cursor.execute('CREATE TABLE IF NOT EXISTS dados(caminho TEXT PRIMARY KEY, nome TEXT, duracao FLOAT)')
 conexao.commit()
 
 def buscar_musicas(diretorio):
-    "buscar por aquivos de musica altomaticamente."
-    listaCaminhoMusica = cursor.execute('SELECT caminho FROM dados').fetchall()
-    for caminhoMusica in listaCaminhoMusica:
-        if not os.path.exists(caminhoMusica):
-            cursor.execute('DELET * FROM dados WHERE caminho = ?', (caminhoMusica,))
+    # Verificando se tem arquivos inexistentes e removendo us
+    lista_caminho_musica = cursor.execute('SELECT caminho FROM dados').fetchall()
+    for caminho_musica in lista_caminho_musica:
+        caminho = caminho_musica[0]
+        if not os.path.exists(caminho):
+            cursor.execute('DELETE FROM dados WHERE caminho = ?', (caminho,))
             conexao.commit()
-    filtros = ('.mp3, .ogg')
+    
+    # Busca por arquivos de música automaticamente
+    filtros = ('.mp3', '.ogg')
     for raiz, _, arquivos in os.walk(diretorio):
         for arquivo in arquivos:
             caminho_arquivo = os.path.join(raiz, arquivo)
-            if any(filtro in arquivo for filtro in filtros) and not cursor.execute('SELECT * FROM dados WHERE caminho = ?', (caminho_arquivo,)).fetchone():
-                cursor.execute('INSERT INTO dados(caminho, nome, duracao) VALUES(?, ?, ?)', (caminho_arquivo, arquivo.split(".")[0], 0.0))
-                conexao.commit()
-                
-buscar_musicas('/home')   
+            if any(filtro in arquivo.lower() for filtro in filtros):
+                if not cursor.execute('SELECT * FROM dados WHERE caminho = ?', (caminho_arquivo,)).fetchone():
+                    cursor.execute('INSERT INTO dados(caminho, nome, duracao) VALUES(?, ?, ?)', (caminho_arquivo, arquivo.split(".")[0], 0.0))
+                    conexao.commit()
+
+buscar_musicas('/home')
+
+ 
