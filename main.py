d2115e541bf7bbe6b9718b1148cd72251e48925c
joao322 <lucasribeiromatos18@gmail.com>
2023-09-03 17:45:24 -0300

    crido o sistema de buscar musicas

--- main.py
+++ main.py
@@ -1,86 +1,31 @@
 import sqlite3
-import playsound
-import customtkinter as ctk
 import os
 
-filtro = ('.mp3', '.ogg')
-janela_size = {'x': 100, 'y': 600}
-
-# Configurando a janela
-janela = ctk.CTk()
-janela.geometry('1000x600')
-janela.title('playsound')
-janela.minsize(600, 400)
-try:
-    conexao = sqlite3.connect('dados/banco-dados.db') 
-
-except sqlite3.OperationalError:
+# Configurando o banco de dados
+caminho_banco = 'dados/banco.db'
+if not os.path.exists(caminho_banco):
     os.makedirs('dados')
-    conexao = sqlite3.connect('dados/banco-dados.db')
-    curso = conexao.cursor()
-    curso.execute("CREATE TABLE dado(caminho_musica Text)")
+conexao = sqlite3.connect(caminho_banco)
 
-# Iniciando a aplicasão    
-class App:
-    def __init__(self) -> None:
-        global scroll
-        ctk.CTkButton(janela, text='arquivos', command=self.menuBurg ,width=30, height=20).place(x=0, y=0)
-        scroll = ctk.CTkScrollableFrame(janela, 180, janela_size['y'] - 30)
-        scroll.place(x=0,y=24)
-        
+cursor = conexao.cursor()
 
-        if curso.execute("SELECT * FROM dados").fetchall() != []:
-        janela.after(10, self.update)      
-        janela.after(10, self.buscar)
-        janela.mainloop()
-        
-    def buscar(self):
-        "buscar por todas as musica do diretorio"
-        
-        diretorio = '/home'
-        notificasao = ctk.CTkLabel(janela, text='o sistema ficara mais lento e quanto buscar por arquivos...')
-        notificasao.place(x=60, y=0)
-        for raiz, *_, arquivos in os.walk(diretorio):
-            for arquivo in arquivos:
-                caminho_completo = os.path.join(raiz, arquivo)
-                # Filtrando os arquivos 
-                if any(exetecao in arquivo for exetecao in filtro) and curso.execute("SELECT * FROM dados WHERE caminho_musica = ?", caminho_completo) == []:
-                    ctk.CTkButton(scroll, text=arquivo, width=175).pack(pady=2, ipadx=10)
-                    curso.execute("INSERT INTO dados VALUES(caminho_musica = ?)", caminho_completo)
-                    conexao.commit()
-                    scroll.update()
-                    
-                    
-        notificasao.place_forget()
-    def update(self):
-        if janela_size['y'] != janela.winfo_height():             
-            janela_size['y'] = janela.winfo_height()
-            scroll.configure(height=janela_size['y'] -30) 
-        janela.after(1, self.update)
-        
-    def fechar_MenuBurg(self, evento):
-        largura = burg.winfo_width() + burg.winfo_x()
-        altura = burg.winfo_height() + burg.winfo_y()
-        if largura < evento.x or altura < evento.y:
-            janela.unbind('<Button-1>')
-            burg.place_forget()
+# Criando a tabla se ela nao existe 
+cursor.execute('CREATE TABLE IF NOT EXISTS dados(caminho TEXT, nome TEXT, duracao FLOAT)')
+conexao.commit()
 
-    def menuBurg(self):
-        global burg
-        janela.bind('<Button-1>', self.fechar_MenuBurg)
-        try: 
-            burg.update()
-        except NameError:
-            burg = ctk.CTkFrame(janela)
-            ctk.CTkButton(burg, 200, text='buscar manualmente').pack(pady=2, ipadx=10)
-            ctk.CTkButton(burg, 200, text='musicas exicluidas').pack(pady=2, ipadx=10)
-            ctk.CTkButton(burg, 200, text='buscar automaticamente', command=self.buscar).pack(pady=2, ipadx=10)
-        else:
-            burg.place_forget()
-
-        burg.place(x=0, y=20)
-        
-
-if __name__ == '__main__':
-    App()
-    janela.mainloop()
+def buscar_musicas(diretorio):
+    "buscar por aquivos de musica altomaticamente."
+    listaCaminhoMusica = cursor.execute('SELECT caminho FROM dados').fetchall()
+    for caminhoMusica in listaCaminhoMusica:
+        if not os.path.exists(caminhoMusica):
+            cursor.execute('DELET * FROM dados WHERE caminho = ?', (caminhoMusica,))
+            conexao.commit()
+    filtros = ('.mp3, .ogg')
+    for raiz, _, arquivos in os.walk(diretorio):
+        for arquivo in arquivos:
+            caminho_arquivo = os.path.join(raiz, arquivo)
+            if any(filtro in arquivo for filtro in filtros) and not cursor.execute('SELECT * FROM dados WHERE caminho = ?', (caminho_arquivo,)).fetchone():
+                cursor.execute('INSERT INTO dados(caminho, nome, duracao) VALUES(?, ?, ?)', (caminho_arquivo, arquivo.split(".")[0], 0.0))
+                conexao.commit()
+                
+buscar_musicas('/home')   
