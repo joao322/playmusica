f91b4f7e7a40bb327d90c90ca7e2e731855419d0
joao322 <lucasribeiromatos18@gmail.com>
2023-09-04 13:01:55 -0300

    adicao do atualiaz a tela

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ conexao = sqlite3.connect(caminho_banco)
 cursor = conexao.cursor()
 
 # Criando a tabela se ela não existe
-cursor.execute('CREATE TABLE IF NOT EXISTS dados(caminho TEXT PRIMARY KEY, nome TEXT, duracao FLOAT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS dados(caminho TEXT], nome TEXT, duracao FLOAT)')
 conexao.commit()
 
 def buscar_musicas(diretorio):
@@ -20,17 +20,26 @@ def buscar_musicas(diretorio):
         if not os.path.exists(caminho):
             cursor.execute('DELETE FROM dados WHERE caminho = ?', (caminho,))
             conexao.commit()
+    tempoAtualizarTela = 6
     
     # Busca por arquivos de música automaticamente
     filtros = ('.mp3', '.ogg')
     for raiz, _, arquivos in os.walk(diretorio):
         for arquivo in arquivos:
             caminho_arquivo = os.path.join(raiz, arquivo)
+            
+            # Atualizar a tela 
+            if tempoAtualizarTela <= 0:
+                tempoAtualizarTela = 6
+                continue
+                
             if any(filtro in arquivo.lower() for filtro in filtros):
                 if not cursor.execute('SELECT * FROM dados WHERE caminho = ?', (caminho_arquivo,)).fetchone():
                     cursor.execute('INSERT INTO dados(caminho, nome, duracao) VALUES(?, ?, ?)', (caminho_arquivo, arquivo.split(".")[0], 0.0))
                     conexao.commit()
-
+            else: 
+                tempoAtualizarTela -=1
+def 
 buscar_musicas('/home')
 
  
---
+++ test.py
@@ -0,0 +1,7 @@
+from pygame import mixer
+mixer.init()
+o = mixer.Sound('High Noon - TrackTribe.mp3')
+o.play()
+mixer.s
+
+
