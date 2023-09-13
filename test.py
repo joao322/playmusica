3e9526f94fc8e7df4f55e840710bdb89c3020fd2
joao322 <lucasribeiromatos18@gmail.com>
2023-09-13 12:53:24 -0300

    s

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
-from pygame import mixer
-mixer.init()
-o = mixer.Sound('High Noon - TrackTribe.mp3')
-o.play()
-mixer.s
+import eyed3
+import playsound
 
+def reproduzirMusica(raquivo):
+    playsound.
 
+def capaMusica(arquivo) -> 'img':
+    audio = eyed3.load(arquivo)
+    if audio.tag:
+        capas = audio.tag.images
+        return capas
+    
+def duracaoMusica(arquivo) -> str:
+    audio = eyed3.load(arquivo)
+
+    duracaoSegundos = audio.info.time_secs
+    duracaoMinutos = int(duracaoSegundos / 60)
+    segundosRestantaes = int(duracaoSegundos % 60)
+    
+    return f'{duracaoMinutos}:{segundosRestantaes}'
